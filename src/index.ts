fb87be94b3e113c87701af698531a4e610d4724c
Robin Franken <franken@lequest.nl>
2018-01-16 08:31:42 +0100

    Imported other events

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import clipboardEvents, {ClipboardEvent} from './clipboard';
-import compositionEvents, {CompositionEvent} from './composition';
+import clipboardEvents, { ClipboardEvent } from './clipboard';
+import compositionEvents, { CompositionEvent } from './composition';
+import keyboardEvents, { KeyboardEvent } from './keyboard';
+import focusEvents, { FocusEvent } from './focus';
+import formEvents, { FormEvent } from './form';
 
-type EventType = ClipboardEvent | CompositionEvent;
+type EventType =
+    | ClipboardEvent
+    | CompositionEvent
+    | KeyboardEvent
+    | FocusEvent
+    | FormEvent;
 
 const allEvents = {
     ...clipboardEvents,
-    ...compositionEvents
+    ...compositionEvents,
+    ...keyboardEvents,
+    ...focusEvents,
+    ...formEvents
 };
 
 const convertEvent = (eventName: EventType) => {
-    return allEvents[eventName];
+    return allEvents[ eventName ];
 };
 
 export default convertEvent;
